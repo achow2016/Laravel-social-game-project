00bee1aacc7cafd5ab28b244c66d42f613cea5f8
alan <tilldusk@gmail.com>
2021-02-16 18:28:30 -0800

    rpg game ui
    add ui "controller" to game component with directional arrows and action buttons
    
    move map dom generation to beforemount() so elements can be manipulated later
    
    at mounted() add classlist toggles to change size of game map grid elements if screen height is small size below 600
    
    add spacing between control elements for larger screen mobile such as iphone x

--- resources/js/components/RpgGame.vue
+++ resources/js/components/RpgGame.vue
@@ -17,17 +17,45 @@
 	
 		<div class="row text-center mt-5 mb-2">
 			<div class="col">
-				<h5>Rpg Game</h5>
+				<h5><!--level number--></h5>
 			</div>
 		</div>
 		
-		<div class="row mt-5 mb-5">
+		<div class="row mt-5 mb-5" id="gridArea">
 			<div class="col">		
 				<div>
 					<div id="mapGrid" class="col text-center">
 						Generating map.
 					</div>					
 				</div>
+			</div>
+		</div>
+		
+		<div class="row mt-5 mb-5" id="controlArea">
+			<div class="col-6">		
+				<div id="directionGrid" class="text-center">
+					<div class="row mb-4 controllerRow">
+						<div class="col-4"><b-icon icon="arrow-up-left-circle"></b-icon></div>
+						<div class="col-4"><b-icon icon="arrow-up-circle"></b-icon></div>
+						<div class="col-4"><b-icon icon="arrow-up-right-circle"></b-icon></div>
+					</div>
+					<div class="row mb-4 controllerRow">
+						<div class="col-4"><b-icon icon="arrow-left-circle"></b-icon></div>
+						<div class="col-4"><b-icon icon="app"></b-icon></div>
+						<div class="col-4"><b-icon icon="arrow-right-circle"></b-icon></div>
+					</div>
+					<div class="row mb-4 controllerRow">
+						<div class="col-4"><b-icon icon="arrow-down-left-circle"></b-icon></div>
+						<div class="col-4"><b-icon icon="arrow-down-circle"></b-icon></div>
+						<div class="col-4"><b-icon icon="arrow-down-right-circle"></b-icon></div>
+					</div>
+				</div>				
+			</div>
+			<div id="actionGrid" class="col-6">
+				<div class="row-9 mb-4 actionRow d-flex justify-content-center">Inspect</div>
+				<div class="row-9 mb-4 actionRow d-flex justify-content-center">Fight</div>
+				<div class="row-9 mb-4 actionRow d-flex justify-content-center">Skill</div>
+				<div class="row-9 mb-4 actionRow d-flex justify-content-center">Loot</div>
 			</div>
 		</div>
 		
@@ -58,7 +86,7 @@
 				mapData: ''
 			}
 		},
-		mounted() { 
+		beforeMount() { 
 			User.getMap({
 					_method: 'POST', token: sessionStorage.getItem('token')
 				}, 
@@ -71,17 +99,18 @@
 					document.getElementById('mapGrid').innerHTML = ""; 
 					for (let i = 0; i < 8; i++) {
 						let row = document.createElement('div');
-						row.classList.add('row');
+						row.classList.add('row', 'mapGridRow');
 						row.setAttribute('id', 'row' + i);
 						document.getElementById('mapGrid').appendChild(row);
 						for (let j = 0; j < 8; j++) {
 							let element = document.createElement('div');
 							element.classList.add('col');
 							element.setAttribute('id', 'row' + i + 'col' + j);
+							
 							if(this.mapData[i][j].terrain == 'grass')
-								element.classList.add('bg-success');
+								element.classList.add('gameGridSquare', 'bg-success', 'pt-2', 'pb-2');
 							else
-								element.classList.add('bg-primary');
+								element.classList.add('gameGridSquare', 'bg-primary', 'pt-2', 'pb-2');
 							
 							if(this.mapData[i][j].treeCover == true) {
 								let treeMarker = document.createTextNode('T');
@@ -95,8 +124,34 @@
 							}
 							document.getElementById('row' + i).appendChild(element);
 						}
-					}	
+					}
+						
+					
 				});
+			
+				
+		},
+		mounted() {
+			//dynamic style fix for small screen
+			//remove large margins around map
+			if(screen.height < 600) {
+				document.getElementById('gridArea').classList.toggle('mb-5');
+				document.getElementById('gridArea').classList.toggle('mt-5');
+				
+				let gridItems = document.getElementsByClassName('gameGridSquare');
+				for(let i = 0; i < gridItems.length; i++) {
+					gridItems[i].classList.toggle('pb-2');
+					gridItems[i].classList.toggle('pt-2');
+				}
+				
+				//set body font size to .8 rem
+				document.getElementsByTagName('body')[0].style.fontSize = '.8rem';
+			}
+
+			if(screen.height > 800) {
+				document.getElementsByTagName('body')[0].style.fontSize = '1.0rem';
+			}				
+			
 		},
 		methods: {
 			moveCharacter(event) {
@@ -132,4 +187,8 @@
 	}
 </script>
 <style scoped>
+	.actionRow {
+		border: 1px solid white;
+	}
+
 </style>
